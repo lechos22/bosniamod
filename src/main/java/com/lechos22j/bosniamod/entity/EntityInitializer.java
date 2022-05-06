7138fd8355e80698dacd9ba81269386013e5d176
Lech <mazurlech8@gmail.com>
2022-05-06 13:52:11 +0200

    Removed renderer package

--- src/main/java/com/lechos22j/bosniamod/entity/EntityInitializer.java
+++ src/main/java/com/lechos22j/bosniamod/entity/EntityInitializer.java
@@ -1,6 +1,8 @@
 package com.lechos22j.bosniamod.entity;
 
 import com.lechos22j.bosniamod.BosniaMod;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -11,10 +13,12 @@ public class EntityInitializer {
             new Identifier(BosniaMod.MOD_ID,"hand_bomb"),
             HandBombEntity.HAND_BOMB_ENTITY_TYPE
         );
+        EntityRendererRegistry.register(HandBombEntity.HAND_BOMB_ENTITY_TYPE, FlyingItemEntityRenderer::new);
         Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier(BosniaMod.MOD_ID,"cluster_bomb"),
             ClusterBombEntity.CLUSTER_BOMB_ENTITY_TYPE
         );
+        EntityRendererRegistry.register(ClusterBombEntity.CLUSTER_BOMB_ENTITY_TYPE, FlyingItemEntityRenderer::new);
     }
 }
